9a460a98a5001d031eb45fd7c01f8695b716cdac
Renan <renandev.backend@gmail.com>
2026-08-18 02:00:26 -0300

    funcao de email e ler email quase prontos

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
+void checkEmail(char *emailDigitado) {
+    printf("Digite seu email: ");
+    scanf("%s", emailDigitado);
+}
 int main() {
-    return 0;
+
+char emailDigitado[30];
+char emailCorreto[4] = "123";
+    do {
+        checkEmail(emailDigitado);
+    } while (strcmp(emailDigitado, emailCorreto) != 0);
+
+   // vai pra parte da senha
 }
