0aed21102b928bb469b520e695e35979c61aa922
Renan <renandev.backend@gmail.com>
2026-08-18 02:48:54 -0300

    programa terminado com sucesso, para futuras melhorias, a main faz 2 funcoes

--- main.c
+++ main.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
+
 void checkEmail(char *emailDigitado) {
     printf("Digite seu email: ");
     scanf("%s", emailDigitado);
 }
-int main() {
 
+void checkSenha(char *senhaDigitada) {
+    printf("Digite sua senha: ");
+    scanf("%s", senhaDigitada);
+}
+
+
+int main() {
 char emailDigitado[30];
 char emailCorreto[4] = "123";
+
     do {
         checkEmail(emailDigitado);
     } while (strcmp(emailDigitado, emailCorreto) != 0);
 
-   // vai pra parte da senha
+char senhaDigitada[30];
+char senhaCorreta[6] = "Rk123";
+
+    do {
+        checkSenha(senhaDigitada);
+    } while (strcmp(senhaDigitada, senhaCorreta) != 0);
+
+    printf("login realizado!\n");
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,14 @@
-1@gmail.com
+
+    do {
+        checkEmail(emailDigitado);
+    } while (strcmp(emailDigitado, emailCorreto) != 0);
+
+char senhaDigitada[30];
+char senhaCorreta[6] = "Rk123";
+
+    do {
+        checkSenha(senhaDigitada);
+    } while (strcmp(senhaDigitada, senhaCorreta) != 0);
+
+    printf("login realizado!\n");
+}
